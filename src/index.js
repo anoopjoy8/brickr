73e12090677e029ecaef4adcc17ebabe9bd8c8fa
Anoop Joy <anoop.joy@techversantinfo.com>
2023-01-24 21:08:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MainContextProvider } from './Context/mainContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <MainContextProvider>
     <App />
-  </React.StrictMode>
+  </MainContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
